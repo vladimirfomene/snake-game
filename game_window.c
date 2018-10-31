73ca938a4ec1dde04b9c822b306d3940aedab9c3
Vladimir Fomene <vladimirfomene@gmail.com>
2018-10-31 06:36:29 +0100

    Implement check collision on square and walls; divide screen into three windows

--- game_window.c
+++ game_window.c
@@ -7,7 +7,10 @@
 *
 **********************************/
 
-#include "game_window.h"
+#include <stdbool.h>
+
+#include "snake.h"
+
 
 
 GameWindow* draw_game_screen(){
@@ -15,82 +18,87 @@ GameWindow* draw_game_screen(){
 	initscr();
 	cbreak();
 	noecho();
-	keypad(stdscr, TRUE);
-	nodelay(stdscr, TRUE);
 	curs_set(FALSE); 
 	start_color(); //enable color attributes
 	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	GameWindow* game_window = setup_game_screen();
-	print_header(game_window);
-	print_footer(game_window);
 	
-	return game_window;
+	return setup_game_screen();
 }
 
 GameWindow* setup_game_screen(){
 	GameWindow* game_window = (GameWindow*) malloc(sizeof(GameWindow));
-	game_window->height = LINES;
+	game_window->height = LINES - 4;
 	game_window->width = COLS;
 	game_window->level = 1;
 	game_window->player_points = 0;
 	game_window->num_squares_per_level = NUM_SQUARES_PER_LEVEL;
+	game_window->canvas = newwin(game_window->height, game_window->width, 2, 0);
+
 
 	return game_window;
 }
 
-void print_header(GameWindow* game_window){
+WINDOW* setup_header(GameWindow* game_window){
+	//Create a window for the header
+	WINDOW* header = newwin(LINES - (LINES - 2), game_window->width, 0, 0);
 
-	attron(COLOR_PAIR(1));
-	printw("SNAKE GAME");
-	attroff(COLOR_PAIR(1));
+	//Get new screen dimensions
+	int header_height, header_width;
+	getmaxyx(header, header_height, header_width);
 	
-	attron(COLOR_PAIR(1));
-	mvprintw(0, (game_window->width / 2) - (strlen("LEVEL 12") / 2), "LEVEL %d", game_window->level);
-	attroff(COLOR_PAIR(1));
+	wmove(header, 0, 0);
 
 	attron(COLOR_PAIR(1));
-	mvprintw(0, (game_window->width - 1) - (strlen("POINTS 185") + PADDING), "POINTS %d", game_window->player_points);
+	wprintw(header, " SNAKE GAME");
+	
+	wmove(header, 0, (header_width / 2) - (strlen("LEVEL 12") / 2));
+	wprintw(header, "LEVEL %d", game_window->level);
+
+	wmove(header, 0, (header_width - 1) - (strlen("POINTS 185") + PADDING));
+	wprintw(header, "POINTS %d", game_window->player_points);
 	attroff(COLOR_PAIR(1));
 
 	//Draw horizontal line
-	move(1, 0);
-	hline('='| COLOR_PAIR(1), game_window->width);
+	wmove(header, 1, 0);
+	whline(header, '='| COLOR_PAIR(1), header_width);
+
+
+	return header;
 }
 
-void print_footer(GameWindow* game_window){
-	//Draw horizontal line
-	move(game_window->height - 2, 0);
-	hline('='| COLOR_PAIR(1), game_window->width);
+WINDOW* setup_footer(GameWindow* game_window){
+	//create a window for the footer
+	WINDOW* footer = newwin(LINES - (LINES - 2), game_window->width, LINES - 2, 0);
+
+
+	//Get new screen dimensions
+	int footer_height, footer_width;
+	getmaxyx(footer, footer_height, footer_width);
+
+	whline(footer, '='| COLOR_PAIR(1), footer_width);
+
+	wmove(footer, 1, 0);
 
 	
 	//Print footer instructions
 	attron(COLOR_PAIR(1));
-	mvprintw(game_window->height - 1, 0, "Press P to pause,  ");
+	
+	wprintw(footer, " Press P to pause,  ");
+
+	wprintw(footer, "Press Q to quit,  ");
+	
+	wprintw(footer, "Press R to resume  ");
+
+	wprintw(footer, "Play with arrow keys");
+
 	attroff(COLOR_PAIR(1));
 
-	attron(COLOR_PAIR(1));
-	printw("Press Q to quit,  ");
-	attroff(COLOR_PAIR(1));
 
-	attron(COLOR_PAIR(1));
-	printw("Press R to resume  ");
-	attroff(COLOR_PAIR(1));
-
-	attron(COLOR_PAIR(1));
-	printw("Play with arrow keys");
-	attroff(COLOR_PAIR(1));
+	return footer;
 
 }
 
-void drop_square(GameWindow* win){
 
-	int random_x = (int) (win->width * (rand() / (double) RAND_MAX));
-	int random_y = (int) (win->height * (rand() / (double) RAND_MAX));
-
-	mvaddch(random_y, random_x, ACS_DIAMOND);
-
-}
-
-void update_screen(){
-	refresh();
+void update_screen(WINDOW* screen){
+	wrefresh(screen);
 }
